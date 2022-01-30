9feee043d86ae7825db7e625ddfe91313d90b1c7
edevars <e.devars.dev@gmail.com>
2022-01-30 14:27:14 -0600

    :tada: Starting project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { GlobalSyles } from '../styles/GlobalStyles'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Todo App</title>
+        <meta name="description" content="Manage your tasks in the easiest way possible" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <GlobalSyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
---
+++ styles/GlobalStyles.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalSyles = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: 'Sora', sans-serif;
+    }
+`
